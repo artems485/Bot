d6bfd401b8f76907f4d1c3d869bd7aedaf6f4b8c
Artem <artems485@gmail.com>
2022-06-10 03:53:56 +0300

    Uninstall logtail

--- index.py
+++ index.py
@@ -2,13 +2,6 @@ import telegram
 from flask import Flask, request
 
 from bot import *
-from logtail import LogtailHandler
-
-handler = LogtailHandler(source_token='DctS4Y1nxhXKroiLgaDKLcgi')
-logger.handlers = []
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
-logger.info('Logtail is ready!!!')
 
 webhook = dispatcher.bot.get_webhook_info()
 if webhook.url:
