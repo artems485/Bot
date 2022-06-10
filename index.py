fdd041316cba3a6bc30aab4c9959e80ae1e40502
Artem <artems485@gmail.com>
2022-06-10 04:23:45 +0300

    Try to add Gmail auth

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import telegram
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect, url_for, session
+from flask_oauth import OAuth
 
 from bot import *
 
@@ -9,12 +10,72 @@ if webhook.url:
 
 dispatcher.bot.set_webhook(url='https://bot-blue-alpha.vercel.app/' + TOKEN)
 
+GOOGLE_CLIENT_ID = '655553482219-gag2tno6u7r7b4rgbjp43vas0qkfo39o.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = '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'
+
+SECRET_KEY = 'asdjaslkdj32kl3j2klj32ljkl2'
+DEBUG = True
+
 app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
+
+google = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=GOOGLE_CLIENT_ID,
+                          consumer_secret=GOOGLE_CLIENT_SECRET)
 
 
 @app.route('/')
-def base():
-    return 'Hi'
+def index():
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+
+    access_token = access_token[0]
+    from urllib.request import Request, urlopen
+    from urllib.error import URLError
+
+    headers = {'Authorization': 'OAuth ' + access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+        return res.read()
+    except URLError as e:
+        # if e.code == 401:
+        #     Unauthorized - bad token
+        # session.pop('access_token', None)
+        # return redirect(url_for('login'))
+        print(e)
+        # return res.read()
+
+
+@app.route('/login')
+def login():
+    callback = url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+
+@app.route('/authorized')
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
 
 
 @app.route('/' + TOKEN, methods=['POST'])
