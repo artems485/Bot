070339482caa95c0a6f56b87b59b6fd1fdec903a
Artem <artems485@gmail.com>
2022-06-10 03:18:00 +0300

    Add Vercel config, test Flask

--- index.py
+++ index.py
@@ -77,11 +77,9 @@ dispatcher.add_handler(conversation)
 app = Flask(__name__)
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route('/')
+def base(path):
+    return 'Hi'
 
-
-updater.start_polling()
-updater.idle()
+# updater.start_polling()
+# updater.idle()
